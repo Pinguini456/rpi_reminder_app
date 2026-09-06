519335b7dd99c3138bd305b13569f6db16c9efd8
Pinguini456 <noahsnachos@gmail.com>
2026-09-06 18:59:28 +1200

    initial commit

--- main.py
+++ main.py
@@ -403,6 +403,7 @@ class CalendarShapeApp:
         self.root.geometry(f"{WIDTH}x{HEIGHT}")
         self.root.attributes("-fullscreen", True)
         self.root.resizable(False, False)
+        self.root.bind("<Escape>", lambda e: self.root.destroy())
         self.root.overrideredirect(True)
 
         # Dark mode toggles via the "4" key (see _on_key_press). The
